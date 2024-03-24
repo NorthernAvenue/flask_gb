cf9d204bacc6f2b4d22eafefe19daa0969465f91
NorthernAvenue <NorthernAvenueAI@gmail.com>
2024-03-24 12:16:29 +0300

    'hw3'

---
+++ create_db.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), unique=True, nullable=False)
+    last_name = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+
+    def __repr__(self):
+        return f"<User {self.username}>"
+
+
+app.app_context().push()
+db.create_all()
--- hw2.py
+++ hw2.py
@@ -1,39 +1,44 @@
-from flask import Flask, render_template, request, redirect, make_response
+from flask import Flask, render_template, request, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+app.config['SECRET_KEY'] = 'super_secret_key'
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def __repr__(self):
+        return f'<User {self.first_name} {self.last_name}>'
 
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        first_name = request.form.get('first_name')
+        last_name = request.form.get('last_name')
+        email = request.form.get('email')
+        password = request.form.get('password')
 
+        hashed_password = generate_password_hash(password)
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    name = request.form.get('name')
-    email = request.form.get('email')
+        new_user = User(first_name=first_name, last_name=last_name, email=email, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
 
-    resp = make_response(redirect('/welcome'))
-    resp.set_cookie('name', name)
-    resp.set_cookie('email', email)
-    return resp
-
-
-@app.route('/welcome')
-def welcome():
-    name = request.cookies.get('name')
-    if name:
-        return render_template('welcome.html', name=name)
-    else:
-        return redirect('/')
-
-
-@app.route('/logout')
-def logout():
-    resp = make_response(redirect('/'))
-    resp.delete_cookie('name')
-    resp.delete_cookie('email')
-    return resp
+    return render_template('register.html')
 
 
 if __name__ == '__main__':
